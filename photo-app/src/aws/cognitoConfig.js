0c5ed925f0e8855c635f76fe7e479d011cc49a30
oussama49 <o0554023267@gmail.com>
2025-07-25 02:02:47 -0400

    Update cognitoConfig.js

--- photo-app/src/aws/cognitoConfig.js
+++ photo-app/src/aws/cognitoConfig.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-  UserPoolId: "us-east-2_ATTuZs7Ih", // 👈 ton UserPool ID
-  ClientId: "3vd2uoaui3bs2igm7cha9uvhc0", // 👈 ton App Client ID
+  UserPoolId: "", // 👈 ton UserPool ID
+  ClientId: "", // 👈 ton App Client ID
 };
 
 export default new CognitoUserPool(poolData);
